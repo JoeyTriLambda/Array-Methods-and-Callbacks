2e12d30e31eebcdd6a35e541b698a704be0ef48a
joseh-roach <joseph-roach@lambdastudents.com>
2021-04-21 15:18:39 -0600

    Finished task1

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import { fifaData } from './fifa.js';
-// Completed this but somehow its deleted smh
+
 // ⚽️ M  V P ⚽️ //
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
 //(a) Home Team name for 2014 world cup final
-
+console.log (fifaData.filter( x => x.Year === 2014 && x.Stage === "Final")[0]['Home Team Name'])
 //(b) Away Team name for 2014 world cup final
-
+console.log (fifaData.filter( x => x.Year === 2014 && x.Stage === "Final")[0]['Away Team Name'])
 //(c) Home Team goals for 2014 world cup final
-
+console.log (fifaData.filter( x => x.Year === 2014 && x.Stage === "Final")[0]['Home Team Goals'])
 //(d) Away Team goals for 2014 world cup final
-
+console.log (fifaData.filter( x => x.Year === 2014 && x.Stage === "Final")[0]['Away Team Goals'])
 //(e) Winner of 2014 world cup final */
-
+let winner = fifaData.filter( x => x.Year === 2014 && x.Stage === "Final")[0];
+if (winner["Home Team Goals"] > winner["Away Team Goals"]){
+    console.log(winner["Home Team Name"])}
+else if (winner["Home Tem Goals"] === winner["Away Team Goals"]
+    git {
+    console.log("Draw")
+}
+else {
+    console.log(winner["Away Team Name"])
+}
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
